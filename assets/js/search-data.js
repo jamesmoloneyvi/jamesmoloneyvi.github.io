e7f0fecd6c7e1155d474ff9127ec9919c58e7cf0
jamesmoloneyvi <jamesmoloneyvi@gmail.com>
2026-02-05 13:52:00 +0000

    Deploying to gh-pages from @ jamesmoloneyvi/jamesmoloneyvi.github.io@dbdc3ee4ea4c449b32c95a3d89fc9980843e3e21 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Projects I am working on.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
